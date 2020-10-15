4aea47a71397a2d2ae1774a706d622960d2b170f
François Grondin <grof2802@MacBook-Pro-de-Francois.local>
2020-10-15 13:51:52 -0400

    Fix bug with argparser

--- download.py
+++ download.py
@@ -7,4 +7,6 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--root', default='', type=str, help='Root to save the datasets')
 parser.add_argument('--folds', default=[1,2,3,4,5,6,7,8,9,10], type=int, nargs='+', help='List of folds')
 
+args = parser.parse_args()
+
 rir = BIRD(root=args.root, folder_in_archive='Bird', folds=args.folds)
